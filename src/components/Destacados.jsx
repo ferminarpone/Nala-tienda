691e7c058e546b6f10bd4c22c91057baadaaccf5
ferminarpone <arponefermin@gmail.com>
2023-06-30 13:19:35 +0200

    Destacados

--- src/components/Destacados.jsx
+++ src/components/Destacados.jsx
@@ -1,9 +1,38 @@
-import { Container, Heading } from "@chakra-ui/react"
+import { Container, Divider, Flex, Grid, GridItem, Image } from "@chakra-ui/react";
+import Scrunchie from '../assets/Scrunchie.png';
+import Anillos from '../assets/Anillos.png';
+import Llavero from '../assets/Llavero osos.png'
+import DestacadosImg from '../assets/Destacados.png'
 
 export const Destacados = () => {
   return (
-    <Container maxWidth="90%" >
-        <Heading  h="300px" textAlign="center">DESTACADOS</Heading>
+    <Container maxWidth="90%" mt="90px">
+      <div className="divTitle">
+        <h2 className="title">DESTACADOS</h2>
+        <Divider
+          orientation="horizontal"
+          color="#4A9A91"
+          border="solid 1px"
+          width="120px"
+          className="dividerTitle"
+        />
+      </div>
+      <Image src={DestacadosImg} alt="Destacados" mt="40px"/>
+
+     {/* Para utilizar todas las imagenes por separado....
+
+      <Grid mt="60px" templateRows="repeat(2, 1fr)" templateColumns="repeat(5, 1fr)" gap={2}>
+        <GridItem rowSpan={1} colSpan={3} bg='tomato' > 
+          <Image src={Scrunchie} alt="Scrunchie"/>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={2} bg='tomato' h="420px">
+          <Image src={Anillos}  h='90%'  w='100%'/>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={5} >
+          <Image src={Llavero}/>
+        </GridItem>
+      </Grid> */}
+
     </Container>
-  )
-}
+  );
+};
