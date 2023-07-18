3d8d6fbda862284c8cd49f33045f75850c3271e5
ferminarpone <arponefermin@gmail.com>
2023-07-18 15:01:29 +0200

    Avances

--- src/components/detalles/ItemDetail.jsx
+++ src/components/detalles/ItemDetail.jsx
@@ -1,4 +1,13 @@
-import { Button, Container, Grid, GridItem, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  Image,
+} from "@chakra-ui/react";
+import { FaRegHeart } from "react-icons/fa";
 
 export const ItemDetail = ({ product }) => {
   return (
@@ -8,11 +17,24 @@ export const ItemDetail = ({ product }) => {
         templateColumns="repeat(2, 1fr)"
         gap={4}
       >
-        <GridItem colSpan={1} rowSpan={3}><Image src={product.img} alt={product.nombre}/></GridItem>
-        <GridItem colSpan={1} rowSpan={1}><h2>{product.nombre}</h2></GridItem>
-        <GridItem><p>Descripción</p></GridItem>
-        <GridItem><Button>HACER PEDIDO POR WHATSAPP</Button> </GridItem>
+        <GridItem colSpan={1} rowSpan={3} >
+          <Image src={product.img} alt={product.nombre} height="80%" w="92%" objectFit="cover" borderRadius="10px"/>
+        </GridItem>
+        <GridItem colSpan={1} rowSpan={1}>
+          <Flex justifyContent="space-between"> 
+            <h2>{product.nombre}</h2>
+            <Button variant="solid" className="buttonCardHeart">
+              <FaRegHeart />
+            </Button>
+          </Flex>
+        </GridItem>
+        <GridItem>
+          <p>Descripción</p>
+        </GridItem>
+        <GridItem bgColor="red">
+          <Button mt="26px">HACER PEDIDO POR WHATSAPP</Button>
+        </GridItem>
       </Grid>
-      </Container>
+    </Container>
   );
 };
