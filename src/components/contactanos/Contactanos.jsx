5214c816158819ab98c66c2fa82a49011e2caf60
ferminarpone <arponefermin@gmail.com>
2023-11-10 21:01:03 +0100

    ContactanosMobile

--- src/components/contactanos/Contactanos.jsx
+++ src/components/contactanos/Contactanos.jsx
@@ -1,42 +1,20 @@
 import "./styles/contactanos.css";
-import { Container, Grid, GridItem, Image } from "@chakra-ui/react";
 import { Footer, NavBar, InfoContactanos, FormContactanos } from "../index"; 
-import Banner from "../../assets/contactanos/Imagen Contactanos.png"; 
 import { useContext } from "react";
 import { FavoritoContext } from "../../context/FavContext";
 import { useScroll } from "../../hooks";
+import { ContactanosMobile } from "./ContactanosMobile";
 
 export const Contactanos = () => {
-  const { isOpen, setIsOpen } = useContext(FavoritoContext);
+  const { isOpen, navExtend } = useContext(FavoritoContext);
   useScroll();
   return (
     <>
       <NavBar />
       <div id="contactanosDesktop">
-        <Container maxWidth="90%" mt="25px">
-          <Grid
-            templateRows="repeat(3, 1fr)"
-            templateColumns="repeat(7, 1fr)"
-            gap={4}
-            className="gridContactanos"
-          >
-            <GridItem rowSpan={3} colSpan={4}>
-              <Image
-                src={Banner}
-                className="bannerContactanos"
-                alt="Gorras Shein"
-              />
-            </GridItem>
-            <GridItem rowSpan={1} colSpan={3}>
-              <InfoContactanos />
-            </GridItem>
-            <GridItem rowSpan={2} colSpan={3}>
-              <FormContactanos />
-            </GridItem>
-          </Grid>
-        </Container>
+        <ContactanosMobile/>
       </div>
-      <div onClick={() => isOpen && setIsOpen(!isOpen)}>
+      <div onClick={navExtend}>
         <div id="contactanosMobile" className={isOpen ? "open" : ""}>
           <h1 className="titleInfo">Contáctanos</h1>
         </div>
